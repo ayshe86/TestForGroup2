9d8cb46ea6b1c8fdc4af36f4ccbc18194b097763
Ayhse Mollamustafa <pek.ayse@gmail.com>
2023-07-10 00:29:50 +0300

    Day2_TC3-4 HomeWork

--- src/test/java/cydeo/ayshe/Day2_TC4.java
+++ src/test/java/cydeo/ayshe/Day2_TC4.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.security.Key;
+
 
 public class Day2_TC4 {
     public static void main(String[] args) {
